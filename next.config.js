d0c079bb3ee56811354ed202c9eafe79d87cf071
nurdin <nurdin@bdg.ebdesk.com>
2023-10-14 00:37:34 +0700

    modify output build app

--- next.config.js
+++ next.config.js
@@ -4,7 +4,9 @@ const nextConfig = {
     sassOptions: {
         includePaths: [path.join(__dirname, 'styles')],
     },
-    output: 'build',
+    output: 'export',
+    distDir: 'build',
+
 }
 
 module.exports = nextConfig
